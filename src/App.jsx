dd92a093963f418b1465124b357a32652f394b42
Mamta <mamtapal9871@gmail.com>
2025-03-19 12:06:49 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import Screenfirst from './component/Screen1/Screenfirst';
 import ScreenSec from './component/Screen2/ScreenSec';
 import './index.css'
 
 const App = () => (
-  <Router>
+  <Router basename='/Ui-design'>
     <Routes>
       <Route path="/" element={<Screenfirst />} />
       <Route path="/second" element={<ScreenSec />} />
